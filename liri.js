9c1b41ebbf2973608e354f0f1806c6c68530b2fa
Vit Benton <vitbenton@Macintosh.local>
2017-08-14 21:29:14 -0400

    added appendTo method for writing to log.txt file with each command, also cleaned up a lot of repeating code into vars and functions

--- liri.js
+++ liri.js
@@ -24,6 +24,48 @@ var songCommand = 'spotify-this-song';
 var twitterCommand = 'my-tweets';
 var doCommand = 'do-what-it-says';
 
+//re-used strings:
+var seperator = "###############################################\r\n\r\n";
+var guideMessage = "Enter '" + movieCommand + "' following a movie title.\r\n" +
+		   	   	   "Enter '" + songCommand + "' following a song name.\r\n" +
+		   		   "Enter '" + twitterCommand + "' to see my 20 most recent tweets.\r\n" +
+		   		   "Enter '" + doCommand + "' to see some custom commands play out.\r\n";
+
+//fuction for console-logging data in addition to appending to log.txt file:
+function log (dataToLog) {
+
+	console.log(dataToLog);
+
+	fs.appendFile('log.txt', dataToLog, 'utf8', function(err){//log pertinent movie info to log.txt
+	 		return console.log(err);
+	});
+}; 
+
+//fuction for querying song with spoitfy module:
+
+function spotifySearch (songName, objectIndex, queryLimit) {
+
+	var spotify = new Spotify({
+	  id: spotConsumerKey,
+	  secret: spotConsumerSecret,
+	});
+
+	spotify
+	  .search({ type: 'track', query: songName, limit: queryLimit })
+	  .then(function(response, error) {
+
+		  var path = response.tracks.items[objectIndex];//capture path for easy re-use
+
+		  var songString = seperator + 
+					   	   "Artist(s): " + path.artists[0].name + "\r\n" +
+					       "Song Name: " + path.name + "\r\n" +
+					       "Preview Link: " + path.href + "\r\n" +
+					       "Album: " + path.album.name + "\r\n\r\n" +
+					       seperator;
+		log(songString);//log pertinent song info to console and log.txt
+	});
+};
+
 //---------------------------------------------------------------
 
 //OMDB:
@@ -32,13 +74,12 @@ if (position2 == movieCommand){//when the movie command is executed ...
 
 	var titleArray = [];
 	var movieTitle = "";
+	var queryURL = "";//API query URL
 
 	for (var i = 3; i < process.argv.length; i++) {//loop through all posiitons to collect title, if title > one word
 		titleArray.push(process.argv[i]);
 		movieTitle = titleArray.join(" ");
 	}
-
-	var queryURL = "";//API query URL
 
 	if (movieTitle == ""){//If no movie title is provided then search the title 'Mr.Nobody'.
 
@@ -67,20 +108,18 @@ if (position2 == movieCommand){//when the movie command is executed ...
 			rottonTomoatoesRating = "Unavailable";
 		};
 
-		 console.log("###############################################\r\n");//log seperator for readability
+		var movieString = seperator + 
+				  	  	  "Movie Title: " + cleanResponse.Title + "\r\n" +
+					  	  "Year: " + cleanResponse.Year + "\r\n" +
+					  	  "IMDB Rating: " + cleanResponse.Ratings[0].Value + "\r\n" + 
+					  	  "Rotton Tomoatoes Rating: " + rottonTomoatoesRating + "\r\n" +
+					  	  "Country: " + cleanResponse.Country + "\r\n" +
+					  	  "Language: " + cleanResponse.Language + "\r\n" +
+					  	  "Plot: " + cleanResponse.Plot + "\r\n" +
+					  	  "Actors/Actresses: " + cleanResponse.Actors + "\r\n\r\n" +
+					  	  seperator;
 
-			console.log(//log pertinent details from OMDB JSON
-		  	  "Movie Title: " + cleanResponse.Title + "\r\n" +
-			  "Year: " + cleanResponse.Year + "\r\n" +
-			  "IMDB Rating: " + cleanResponse.Ratings[0].Value + "\r\n" + 
-			  "Rotton Tomoatoes Rating: " + rottonTomoatoesRating + "\r\n" +
-			  "Country: " + cleanResponse.Country + "\r\n" +
-			  "Language: " + cleanResponse.Language + "\r\n" +
-			  "Plot: " + cleanResponse.Plot + "\r\n" +
-			  "Actors/Actresses: " + cleanResponse.Actors + "\r\n"
-			);
-
-		  	console.log("###############################################");//log seperator for readability
+		log(movieString);//log pertinent movie info to console and log.txt
 
 	});
 
@@ -100,54 +139,15 @@ else if (position2 == songCommand){//when the movie command is executed ...
 		song = songArray.join(" ");//clean up songArray if song title contains multiple words
 	}
 
-	var spotify = new Spotify({
-	  id: spotConsumerKey,
-	  secret: spotConsumerSecret,
-	});
-
 	if (song == ""){//If no song is provided then query "The Sign" by Ace of Base.
 
-		spotify
-		  .search({ type: 'track', query: "Ace of Base", limit: 3 })
-		  .then(function(response, error) {
-
-		  var path = response.tracks.items[2];//capture path for easy re-use
-
-		console.log("###############################################\r\n");//log seperator for readability
-
-			console.log(//log pertinent details from OMDB JSON
-		  	  "Artist(s): " + path.artists[0].name + "\r\n" +
-			  "Song Name: " + path.name + "\r\n" +
-			  "Preview Link: " + path.href + "\r\n" +
-			  "Album: " + path.album.name + "\r\n"
-
-			);
-
-		console.log("###############################################");//log seperator for readability
-
-		});
+		spotifySearch('Ace of Base', 2, 3);
 	}
 
 	else {
-		spotify
-		  .search({ type: 'track', query: song, limit: 1 })
-		  .then(function(response, error) {
 
-		  var path = response.tracks.items[0];//capture path for easy re-use
+		spotifySearch(song, 0, 1);
 
-		console.log("###############################################\r\n");//log seperator for readability
-
-			console.log(//log pertinent details from OMDB JSON
-		  	  "Artist(s): " + path.artists[0].name + "\r\n" +
-			  "Song Name: " + path.name + "\r\n" +
-			  "Preview Link: " + path.href + "\r\n" +
-			  "Album: " + path.album.name + "\r\n"
-
-			);
-
-		console.log("###############################################");//log seperator for readability
-
-		});
 	};
 }
 
@@ -166,9 +166,9 @@ else if (position2 == twitterCommand){//when the twitter command is executed ...
 
 	client.get('statuses/user_timeline', function(error, tweets) {
 
-		if(error) throw error;
+		if (error) throw error;
 
-		console.log("###############################################\r\n");//log seperator for readability
+		log(seperator);//log seperator for readability
 
 	    for (i = 0; i < tweets.length ; i++) {//loop through each position of the tweet array
 
@@ -176,16 +176,16 @@ else if (position2 == twitterCommand){//when the twitter command is executed ...
 
 	    	if (i === 0) {//if tweet #1 is posted, clarify that this is the most recent tweet
 
-	    		console.log("Tweet #1 (Most Recent): " + tweets[i].text + "\r\n");
+	    		log("Tweet #1 (Most Recent): " + tweets[i].text + "\r\n");
 	    	}
 
 	    	else {//otherwise list normally
 
-	    		console.log("Tweet #" + tweetNum + ": " + tweets[i].text + "\r\n");
+	    		log("Tweet #" + tweetNum + ": " + tweets[i].text + "\r\n");
 	    	};
 	    }
 
-		 console.log("###############################################\r\n");//log seperator for readability
+		 log(seperator);//log seperator for readability
 
 	});
 
@@ -205,43 +205,17 @@ else if (position2 == doCommand){//when the do command is executed ...
 
 	   var dataArray = data.split(',');//convert string in .txt file to array
 
-	   for (i = 0; i < dataArray.length; i++) {//loop through dataArray
+	    for (i = 0; i < dataArray.length; i++) {//loop through dataArray
 
 	   		var keyword = dataArray[i + 1];//capture query keyword which should always follow command in the array
 
 	   		if (dataArray[i] == songCommand){//if the song command is found, initiate spotify module
 
-	   			var spotify = new Spotify({
-				  id: spotConsumerKey,
-				  secret: spotConsumerSecret,
-				});
+	   			spotifySearch(keyword, 0, 1);
 
-	   			spotify
-				  .search({ type: 'track', query: keyword, limit: 1 })
-				  .then(function(response, error) {
-
-				  var path = response.tracks.items[0];//capture path for easy re-use
-
-				console.log("###############################################\r\n");//log seperator for readability
-
-					console.log(//log pertinent details from OMDB JSON
-				  	  "Artist(s): " + path.artists[0].name + "\r\n" +
-					  "Song Name: " + path.name + "\r\n" +
-					  "Preview Link: " + path.href + "\r\n" +
-					  "Album: " + path.album.name + "\r\n"
-
-					);
-
-				console.log("###############################################");//log seperator for readability
-
-				});
-
-	   		};
-
-	   };
-
+	  		 };
+		};
 	});
-
 }
 
 //---------------------------------------------------------------
@@ -250,16 +224,11 @@ else if (position2 == doCommand){//when the do command is executed ...
 
 else if (position2 === undefined){//when the do command is executed ...
 
-	   console.log("###############################################\r\n");//log seperator for readability
+	   console.log(seperator);//log seperator for readability
 
-	   console.log("... but you didn't enter a command. Please choose from the following commands:\r\n" + 
-	   	"Enter '" + movieCommand + "' following a movie title.\r\n" +
-	   	"Enter '" + songCommand + "' following a song name.\r\n" +
-	   	"Enter '" + twitterCommand + "' to see my 20 most recent tweets.\r\n" +
-	   	"Enter '" + doCommand + "' to see some custom commands play out.\r\n"
-	   	);
+	   console.log("... but you didn't enter a command. Please choose from the following commands:\r\n" + guideMessage);
 
-	   console.log("###############################################\r\n");//log seperator for readability
+	   console.log(seperator);//log seperator for readability
 
 }
 
@@ -267,15 +236,12 @@ else if (position2 === undefined){//when the do command is executed ...
 
 else {//when something that isn't in the list of commands is entered ...
 
-	   console.log("###############################################\r\n");//log seperator for readability
+	   console.log(seperator);//log seperator for readability
 
-	   console.log("You entered an incorrect command. Please choose from the following commands:\r\n" + 
-	   	"Enter '" + movieCommand + "' following a movie title.\r\n" +
-	   	"Enter '" + songCommand + "' following a song name.\r\n" +
-	   	"Enter '" + twitterCommand + "' to see my 20 most recent tweets.\r\n" +
-	   	"Enter '" + doCommand + "' to see some custom commands play out.\r\n"
-	   	);
+	   console.log("You entered an incorrect command. Please choose from the following commands:\r\n" + guideMessage);
 
-	   console.log("###############################################\r\n");//log seperator for readability
+	   console.log(seperator);//log seperator for readability
 
 };
+
+//---------------------------------------------------------------END
